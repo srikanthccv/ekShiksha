a55d3990504b10a8951c4f6d3f60ebacc32020f0
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2019-03-01 20:21:39 +0530

    add function for route /lattice/<structure>

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def ptable():
     return render_template('ptable.html')
 
+@app.route('/lattice/<structure>')
+def lattice_structure(structure):
+    return render_template('lattice_structure.html', structure=structure)
+
 if __name__ == '__main__':
     app.run(debug=True)
