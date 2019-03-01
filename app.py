ebb6f6f4d167aea9a09c3ac5ae7fcf6a92e8f4dc
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2019-03-01 23:43:15 +0530

    kids are stupid

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/ptable')
+@application.route('/ptable')
 def ptable():
     return render_template('ptable.html')
 
-@app.route('/lattice/<structure>')
+@application.route('/lattice/<structure>')
 def lattice_structure(structure):
     return render_template('lattice_structure.html', structure=structure)
 
-@app.route('/geometry/<model>')
+@application.route('/geometry/<model>')
 def geometric_model(model):
     return render_template('geometric_model.html', model=model)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
