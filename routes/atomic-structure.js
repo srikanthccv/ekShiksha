22af7f8f9ad652b57c161969f88e72b4b19ab34b
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2019-05-20 13:37:02 +0530

    + atomic-structure route

---
+++ routes/atomic-structure.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/', function(request, response) {
+    response.render('atomic_structure');
+});
+
+router.get('/atom/:atomic_number', function(request, response) {
+    let atomic_number = request.params.atomic_number;
+    response.render('atomic_view', {atomic_number: atomic_number});
+});
+
+module.exports = router;
