2603ae3f720cff58a9629e42c07ebae6686f1f88
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2019-06-08 16:23:25 +0530

    Set attribute targe=_blank for topics

--- public/js/client/index.js
+++ public/js/client/index.js
@@ -1,35 +1,36 @@
-const topicsList = document.getElementById('topics');
+const topicsList = document.getElementById("topics");
 const topicsLink = {
-    1 : "/periodic-table",
-    2 : "/geometry/linear",
-    3 : "/solid-state/cubic",
-    4 : "/chemical-reactions/combination",
-    5 : "/hybridization/sp",
-    6 : "/organic-reactions/sn1",
+  1: "/periodic-table",
+  2: "/geometry/linear",
+  3: "/solid-state/cubic",
+  4: "/chemical-reactions/combination",
+  5: "/hybridization/sp",
+  6: "/organic-reactions/sn1"
 };
 
 const getTopicsListener = function() {
-    topics = JSON.parse(this.responseText);
-    topics.forEach(function(topic) {
-        appendNewTopic(topic);
-    });
-}
+  topics = JSON.parse(this.responseText);
+  topics.forEach(function(topic) {
+    appendNewTopic(topic);
+  });
+};
 
 const appendNewTopic = function(topic) {
-    const head3 = document.createElement('h3');
-    const newTopicItem = document.createElement('li');
-    const link = document.createElement('a');
-    link.setAttribute('href', topicsLink[topic.topic_id]);
-    link.innerHTML = topic.topic_name;
-    newTopicItem.appendChild(link);
-    head3.appendChild(newTopicItem);
-    const p = document.createElement('p');
-    p.innerHTML = topic.description;
-    topicsList.appendChild(head3);
-    topicsList.appendChild(p);
-}
+  const head3 = document.createElement("h3");
+  const newTopicItem = document.createElement("li");
+  const link = document.createElement("a");
+  link.setAttribute("href", topicsLink[topic.topic_id]);
+  link.setAttribute("target", "_blank");
+  link.innerHTML = topic.topic_name;
+  newTopicItem.appendChild(link);
+  head3.appendChild(newTopicItem);
+  const p = document.createElement("p");
+  p.innerHTML = topic.description;
+  topicsList.appendChild(head3);
+  topicsList.appendChild(p);
+};
 
 const topicsRequest = new XMLHttpRequest();
 topicsRequest.onload = getTopicsListener;
-topicsRequest.open('get', '/topics');
+topicsRequest.open("get", "/topics");
 topicsRequest.send();
